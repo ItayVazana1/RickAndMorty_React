c1c18442caede5dbd38b583fe8011e4120291104
Your Name <you@example.com>
2023-09-30 01:30:19 +0300

    30/9-update

--- src/Pages/WatchListPage.js
+++ src/Pages/WatchListPage.js
@@ -1,43 +1,53 @@
 import React, { useState,useEffect } from 'react'
 import Pagination from '../components/Pagination.js';
 
-
-
-
 function WatchListPage() {
-   // load all characters data
+
    let [pageNumber , setPageNumber] = useState(1);
    let [fetchedData , updateFetchedData] =useState([]);
-   let { info , results } = fetchedData;
+   let {results} = fetchedData;
    let api =  `https://rickandmortyapi.com/api/episode?page=${pageNumber}`;
  
-   useEffect(()=>{
-       (async function(){
-           let data = await fetch(api).then((res) => res.json());
-           console.log(data.results);
-           updateFetchedData(data);
-         })()},[api]);
+    useEffect(() => {
+        (async function(){
+            let data = await fetch(api).then((res) => res.json());
+            updateFetchedData(data);
+          })()},[api]);
+          
+          console.log(results);
+         return(
+                <div>
+                    <div class="container">
+                        <div class="row p-4">
+                        <div class="col-7">
+                            <table class="table table-dark">
+                            <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col" style={{color: 'GreenYellow'}}>Episode Code</th>
+                                <th scope="col" style={{color: 'GreenYellow'}}>Episode Name</th>
+                                <th scope="col" style={{color: 'GreenYellow'}}>Air Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                            <th scope="row"><input class="form-check-input" type="checkbox" value="" id="CheckBox"></input></th>
+                            <td>Its</td>
+                            <td>On</td>
+                            <td>My TaskList..</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                        <div class="col-5">
+                        <h1>Column 2</h1>
+                        </div>
+                    </div>
+                    </div>
+                    <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} maxPages={3}/>
+                </div>
 
-         let display;
-         if(results){
-             display = results.map(x=>{
-                 let {id , name , status ,  species , gender , origin , location , image ,episode , url , created } = x;
-                 return (
-                 <div>
-                  <p>{x.episode}  -  "{x.name}"    -   {x.air_date}</p>
-                 </div>
-                 );
-             });
-         }else{
-             display = "404 - Not Found :("
-         }
-         return <div>
-         
-            <>{display}</>
-            <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} maxPages={3}/>
-          </div>
-
-
+         )
     }
 
 export default WatchListPage;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import { useState , useEffect} from 'react';
 
 const MODAL_STYLES = {
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%,-50%)',
-    backgroundColor: '#FFF',
-    paddingLeft: '200px',
-    paddingRight: '200px',
-    paddingTop: '100px',
-    paddingBottom: '100px',
+    backgroundImage: `url("https://i.pinimg.com/564x/62/97/71/629771374d3a362cbff3eb3eb21d7995.jpg")`,
+    backgroundSize: 'cover' ,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    width:'550px',
+    height: '550px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+    paddingBottom: '50px',
+    paddingTop:'75px',
     zIndex: 1000,
     color:'#482a4a',
-    border: '4px solid purple',
-    borderBottomLeftRadius: '30px',
-    borderBottomRightRadius: '30px',
-    borderTopLeftRadius: '30px',
-    borderTopRightRadius: '30px'
+    border: '4px solid white',
+    borderBottomLeftRadius: '400px',
+    borderBottomRightRadius: '400px',
+    borderTopLeftRadius: '400px',
+    borderTopRightRadius: '400px',
+    fontFamily: 'Acme'
 }
 
 const OVERLAY_STYLE = {
@@ -30,28 +36,106 @@ const OVERLAY_STYLE = {
 }
 
 const CLOSE_BTN_STYLE = {
-    position: 'fixed',
-    bottom:15,
-    left:20
+    position: 'relative',
+    bottom:-25,
+    width:'150px',
+    fontSize: 25
 }
 
+const PIC_Style = {
 
-function Modal({Open,children,onClose,Cdata}) {
+    border: '2px',
+    width: '220px',
+    borderBottomLeftRadius: '30px',
+    borderBottomRightRadius: '30px',
+    borderTopLeftRadius: '30px',
+    borderTopRightRadius: '30px',
+}
+
+const LABLE_Style = {
+        backgroundColor: 'GhostWhite',
+        color: 'indigo',
+        borderBottomLeftRadius: '10px',
+        borderBottomRightRadius: '10px',
+        borderTopLeftRadius: '10px',
+        borderTopRightRadius: '10px',
+        paddingLeft: '15px',
+        paddingRight: '15px',
+        paddingTop: '2px',
+        paddingBottom: '2px',
+        fontFamily: 'Oswald',
+        fontSize: 16
+        
+    };
+    
+
+function Modal({Open,onClose,Cdata}) {
     if(Open == false) return null;
+
+    const [isHover, setIsHover] = useState(false);
+
+    const handleMouseEnter = () => {
+       setIsHover(true);
+    };
+    const handleMouseLeave = () => {
+       setIsHover(false);
+    };
+
+
+    const LINK_STYLE = {
+        position:'relative',
+        backgroundColor: isHover ? 'Tomato' : 'indigo',
+        color: isHover ? 'Snow' : 'SpringGreen',
+        borderBottomLeftRadius: '10px',
+        borderBottomRightRadius: '10px',
+        borderTopLeftRadius: '10px',
+        borderTopRightRadius: '10px',
+        paddingLeft: '15px',
+        paddingRight: '15px',
+        paddingTop: '6px',
+        paddingBottom: '6px',
+        bottom: -30,
+        fontSize: 20
+        
+    };    
+
+    const [FEdata, setFEdata] = useState([]);
+    useEffect(() => {
+       fetch(Cdata.episode[0])
+          .then((response) => response.json())
+          .then((data) => {
+             setFEdata(data);
+          })
+          .catch((err) => {
+             console.log(err.message);
+          });
+    }, []);
+
     return (
+        
         <>
         <div style={OVERLAY_STYLE}/>
-        <div style={MODAL_STYLES}>
-        <img src={Cdata.image}/>
-            {Cdata.id}<br></br>
-            {Cdata.name}<br></br>
-            {Cdata.status}<br></br>
-            {Cdata.gender}<br></br>
-            {Cdata.species}<br></br>
-            {Cdata.location.name}<br></br>
-            {Cdata.origin.name}<br></br>
-            <a href={Cdata.url}><b>Character unique API url</b></a>
-            <button className='btn btn-danger' style={CLOSE_BTN_STYLE} onClick={onClose}><b>Close</b></button>
+        <div style={MODAL_STYLES} className='container'>
+        <div className='row p-4' ></div>
+            <div className='row'>
+                <div className='col' align='center'>
+                <span style={LABLE_Style} ><b>ID:</b><text style={{paddingLeft: '10px'}}>#{Cdata.id}</text></span><br></br><br></br>
+                <span style={LABLE_Style} ><b>Current Status:</b><text style={{paddingLeft: '10px'}}>{Cdata.status}</text></span><br></br><br></br>
+                <span style={LABLE_Style} ><b>Gender:</b><text style={{paddingLeft: '10px'}}>{Cdata.gender}</text></span><br></br><br></br>
+                <span style={LABLE_Style} ><b>Species:</b><text style={{paddingLeft: '10px'}}>{Cdata.species}</text></span><br></br><br></br>
+                <span style={LABLE_Style} ><b>Origin:</b><text style={{paddingLeft: '10px'}}>{Cdata.origin.name}</text></span><br></br><br></br>
+                <span style={LABLE_Style} ><b>First Appearance:</b><text style={{paddingLeft: '10px'}}>{FEdata.episode}</text></span><br></br><br></br>
+               
+                </div>
+                <div className='col'>
+                    <img src={Cdata.image} style={PIC_Style}/>
+                    <button className='btn btn-danger btn-lg' style={CLOSE_BTN_STYLE} onClick={onClose}><b>Close</b></button>
+
+                </div>
+                
+            </div>
+            <a href={Cdata.url} style={LINK_STYLE} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><b>Character API</b></a>
+
         </div>
         </>
     );
