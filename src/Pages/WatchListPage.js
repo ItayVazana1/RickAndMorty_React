1a6edd9d9a8ae5623759337515cbf74c12822853
Your Name <you@example.com>
2023-09-30 20:13:41 +0300

    almost done

--- src/Pages/WatchListPage.js
+++ src/Pages/WatchListPage.js
@@ -1,51 +1,98 @@
 import React, { useState,useEffect } from 'react'
 import Pagination from '../components/Pagination.js';
 
-function WatchListPage() {
 
-   let [pageNumber , setPageNumber] = useState(1);
-   let [fetchedData , updateFetchedData] =useState([]);
-   let {results} = fetchedData;
-   let api =  `https://rickandmortyapi.com/api/episode?page=${pageNumber}`;
- 
+
+function Add_to_WatchList(item){
+
+
+}
+
+function Remove_from_WatchList(){
+    
+}
+
+function WatchListPage() {
+    const [pageNumber, setPageNumber] = useState(1);
+    const [fetchedData, updateFetchedData] = useState({ results: [] }); // Initialize results as an empty array
+    const { results } = fetchedData;
+    const api = `https://rickandmortyapi.com/api/episode?page=${pageNumber}`;
+  
     useEffect(() => {
-        (async function(){
-            let data = await fetch(api).then((res) => res.json());
-            updateFetchedData(data);
-          })()},[api]);
-          
-          console.log(results);
-         return(
-                <div>
-                    <div class="container">
-                        <div class="row p-4">
-                        <div class="col-7">
-                            <table class="table table-dark">
-                            <thead>
-                            <tr>
-                                <th scope="col"></th>
-                                <th scope="col" style={{color: 'GreenYellow'}}>Episode Code</th>
-                                <th scope="col" style={{color: 'GreenYellow'}}>Episode Name</th>
-                                <th scope="col" style={{color: 'GreenYellow'}}>Air Date</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                            <th scope="row"><input class="form-check-input" type="checkbox" value="" id="CheckBox"></input></th>
-                            <td>Its</td>
-                            <td>On</td>
-                            <td>My TaskList..</td>
-                            </tr>
-                            </tbody>
-                            </table>
-                        </div>
-                        <div class="col-5">
-                        <h1>Column 2</h1>
-                        </div>
-                    </div>
-                    </div>
-                    <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} maxPages={3}/>
-                </div>
+      async function fetchData() {
+        try {
+          const response = await fetch(api);
+          const data = await response.json();
+          updateFetchedData(data);
+        } catch (error) {
+          // Handle fetch errors here
+          console.error("Error fetching data:", error);
+        }
+      }
+  
+      fetchData();
+    }, [api]);
+  
+    
+    const tb_data = results.map((item) => {
+      return (
+        <tr key={item.id}> {/* Make sure to add a unique key */}
+          <td>
+            <button className='btn btn-sm btn-success' style={{ fontFamily: 'Acme', fontSize: '13px', height: '40px'}} onClick={Add_to_WatchList(item)}>
+              Add
+            </button>
+          </td>
+          <td style={{ fontFamily: 'Acme', fontSize: '13px' }}>{item.episode}</td>
+          <td style={{ fontFamily: 'Acme', fontSize: '13px' }}>{item.name}</td>
+          <td style={{ fontFamily: 'Acme', fontSize: '13px' }}>{item.air_date}</td>
+        </tr>
+      );
+    });
+  
+    return (
+      <div>
+        <div className="container">
+          <div className="row p-4">
+            <div className="col-7">
+              <table className="table table-dark">
+                <thead>
+                  <tr>
+                    <th scope="col"></th>
+                    <th scope="col" style={{ color: 'GreenYellow' }}>
+                      Episode Code
+                    </th>
+                    <th scope="col" style={{ color: 'GreenYellow' }}>
+                      Episode Name
+                    </th>
+                    <th scope="col" style={{ color: 'GreenYellow' }}>
+                      Air Date
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>{tb_data}</tbody>
+              </table>
+            </div>
+            <div className="col-5">
+              <table className="table" id='WatchList'>
+                <thead className="thead">
+                  <tr>
+                    <th scope="col"></th>
+                    <th scope="row"><h2><b>My Watch List</b></h2></th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="col"><button className='btn btn-sm btn-danger' style={{fontFamily: 'Acme', fontSize: '13px', height: '40px'}}>
+                           Remove</button></td>
+                        <td scope="col">name of somthing</td>
+                    </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} maxPages={3}/>
+    </div>
 
          )
     }
